bc3197a76d8490a428009f0b507d8dd02561c232
Niko Matsakis <nikomat@amazon.com>
2024-10-03 22:54:45 +0000

    create a `cargo duchess` utility

--- duchess-build-rs/src/lib.rs
+++ duchess-build-rs/src/lib.rs
@@ -9,6 +9,20 @@ mod java_compiler;
 mod re;
 mod shim_writer;
 
+/// Build Rs configuration for duchess.
+/// To use duchess you must invoke [`DuchessBuildRs::execute`][].
+///
+/// # Example
+///
+/// The simplest build.rs is as follows.
+///
+/// ```rust
+/// use duchess_build_rs::DuchessBuildRs;
+///
+/// fn main() -> anyhow::Result<()> {
+///     DuchessBuildRs::new().execute()?;
+/// }
+/// ```
 pub struct DuchessBuildRs {
     src_path: PathBuf,
 }
@@ -22,15 +36,24 @@ impl Default for DuchessBuildRs {
 }
 
 impl DuchessBuildRs {
+    /// Create a new DuchessBuildRs instance.
+    /// Equivalent to `DuchessBuildRs::default()`.
     pub fn new() -> Self {
         Self::default()
     }
 
+    /// Configure the path where Rust sources are found.
+    /// The default is `.`.
+    /// We will automatically search all subdirectories for `.rs` files.
     pub fn src_path(mut self, src_path: PathBuf) -> Self {
         self.src_path = src_path;
         self
     }
 
+    /// Execute the duchess `build.rs` processing.
+    ///
+    /// Detects uses of duchess build macros and derives
+    /// and generates necessary support files in the `OUT_DIR` side.
     pub fn execute(self) -> anyhow::Result<()> {
         let compiler = &JavaCompiler::new()?;
         for rs_file in files::rs_files(&self.src_path) {
